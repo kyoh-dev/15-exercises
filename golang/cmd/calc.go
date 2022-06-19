6c7d450df46b9abf168e7b0efc2b0330fc3b605e
Connor Keogh <contact@kyoh.run>
2022-06-19 16:58:52 +1000

    feature: Add product and mean subcommands

--- golang/cmd/calc.go
+++ golang/cmd/calc.go
@@ -6,11 +6,14 @@ import (
 	"kyoh-dev/15-exercises/internal/p1"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	sumCmd := flag.NewFlagSet("sum", flag.ExitOnError)
 	prodCmd := flag.NewFlagSet("product", flag.ExitOnError)
+	meanCmd := flag.NewFlagSet("mean", flag.ExitOnError)
+	sqrtCmd := flag.NewFlagSet("sqrt", flag.ExitOnError)
 	if len(os.Args) < 2 {
 		fmt.Println("ERR: expected a subcommand: [sum, product, mean, sqrt]")
 		os.Exit(1)
@@ -44,6 +47,32 @@ func main() {
 			fmt.Println("ERR: expected a sequence of space-separated integers")
 			os.Exit(1)
 		}
+	case "mean":
+		err := meanCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		nums := meanCmd.Args()
+		if nums != nil {
+			n := p1.StrToIntSlice(&nums)
+			m := p1.Mean(n)
+			fmt.Printf("MEAN: %d\n", m)
+		} else {
+			fmt.Println("ERR: expected a sequence of space-separated integers")
+			os.Exit(1)
+		}
+	case "sqrt":
+		err := sqrtCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		num := os.Args[2]
+		i, err := strconv.Atoi(num)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := p1.Sqrt(float64(i))
+		fmt.Printf("SQUARE ROOT: %f\n", s)
 	default:
 		fmt.Println("ERR: expected a subcommand: [sum, product, mean, sqrt]")
 		os.Exit(1)
